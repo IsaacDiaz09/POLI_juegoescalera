59bddf900e6fdf681646a4e1d510905e4472d0e1
Isaac Diaz Perez <ivvidp12@gmail.com>
2025-06-10 19:19:54 -0500

    feat: se agrega configuracion maven para ejecucion de tests

---
+++ src/test/java/dev/poli/students/game/MainTest.java
@@ -0,0 +1,13 @@
+package dev.poli.students.game;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    @Test
+    void testsLoaded() {
+        Assertions.fail("Pendiente!!!");
+    }
+
+}
